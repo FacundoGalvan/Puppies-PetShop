531a174ed7fe97469a8f3cde136bf61f785f9894
Facundo <facundogalvan097@gmail.com>
2022-03-07 16:25:21 -0300

    por si las

--- src/components/navBar/navBar.js
+++ src/components/navBar/navBar.js
@@ -5,7 +5,7 @@ import {Link} from 'react-router-dom'
 import { CartWidget } from "../CartWidget/CartWidget"
 import bannertwo from '../img/envioprogramado.png'
 import "./navBar.scss"
-import bannermarcas from "../img/bannermarcas.png"
+
 
 export const NavBarOne = () => {
     return (
@@ -18,9 +18,7 @@ export const NavBarOne = () => {
                           <Navbar.Toggle aria-controls="basic-navbar-nav"/>
                           <Navbar.Collapse id="basic-navbar-nav">
                           <Nav className="me-auto">
-                             <NavDropdown title="Ayuda" id="basic-nav-dropdown">
-                                 <NavDropdown.Item href="#action/3.1">22-4444-3334</NavDropdown.Item>
-                            </NavDropdown>
+                            <Nav.Link className="Ayuda" href="#home">Ayuda</Nav.Link>
                             <Nav.Link className="miCuenta" href="#home">Mi Cuenta</Nav.Link>
                             <CartWidget/>
                            </Nav>
@@ -29,11 +27,11 @@ export const NavBarOne = () => {
             </Navbar>
                           <Container className="Contenedorbotton">
                           <ButtonGroup className="MenudeBotones" >
-                          <Link to='/productos/mascotas' ><Button>Mascotas</Button> </Link> 
-                          <Link to ="/productos/marcas"><Button>Marcas</Button></Link>
-                          <Link to='/productos/servicios'><Button >Servicios</Button></Link>
-                          <Link to='/productos/puppisone'><Button>Puppis One</Button></Link>
-                          <Link to='/productos/sucursales'><Button>Sucursales</Button></Link>
+                          <Link to='/productos/mascotas' ><Button>Alimentos</Button> </Link> 
+                          <Link to ="/productos/marcas"><Button>Purina</Button></Link>
+                          <Link to='/productos/servicios'><Button >Royal Canin</Button></Link>
+                          <Link to='/productos/puppisone'><Button>Old Prince</Button></Link>
+                          <Link to='/productos/sucursales'><Button>Vital Can</Button></Link>
                           </ButtonGroup>
                           </Container>
                 <div className="envioprogr" >
